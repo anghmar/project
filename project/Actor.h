275539d67d6e1837e9d4b37aa42defa50126dade
pjetka <DANTEM@NTB110710>
2024-02-07 15:46:22 +0100

    Added actor.h and GameObject.h

---
+++ project/Actor.h
@@ -0,0 +1,36 @@
+#pragma once
+#include <vector>
+#include "game.h"
+
+class Actor
+{
+public:
+	//To track state of actor
+	enum State
+	{
+		EActive,
+		EPaused,
+		EDead
+	};
+	Actor(class Game* game);
+	virtual ~Actor();
+
+	void Update(float deltaTime);
+	void UpdateComponents(float deltaTime);
+	virtual void UpdateActor(float deltaTime);
+
+	//getters
+	// TODO
+	//setters
+	// TODO
+
+	void AddComponent(class Component* component);
+	void RemoveComponent(class Component* component);
+private:
+	State mState;
+	Vector2 mPosition;
+	float mScale;
+	float mRotation;
+	std::vector<class Component*> mComponents;
+	class Game* mGame;
+};
---
+++ project/GameObject.h
@@ -0,0 +1,11 @@
+#pragma once
+
+
+//class GameObject
+//{
+//public:
+//	void AddComponent(Component* comp);
+//	void RemoveComponent(Component* comp);
+//private:
+//	std::unordered_set<Component*> mComponents;
+//};
