75d4ccfb00404e76b248df94bf72113cab954350
pjetka <DANTEM@NTB110710>
2024-09-13 15:51:47 +0200

    ended at page 230.. added LoadBank, UnloadBank and UnloadAllBanks

--- project/AudioSystem.cpp
+++ project/AudioSystem.cpp
@@ -42,6 +42,9 @@ bool AudioSystem::Initialize()
 
     mSystem->getCoreSystem(&mCoreSystem);
 
+    LoadBank("Assets/Master Bank.strings.bank");
+    LoadBank("Assets/Master Bank.bank");
+
     return true;
 }
 
@@ -53,4 +56,125 @@ void AudioSystem::Shutdown()
 void AudioSystem::Update(float deltaTime)
 {
     mSystem->update();
+}
+
+void AudioSystem::LoadBank(const std::string& name)
+{
+    //Prevent double loading
+    if (mBanks.find(name) != mBanks.end())
+    {
+        return;
+    }
+
+    //Try to load bank
+    FMOD::Studio::Bank* bank = nullptr;
+    FMOD_RESULT result = mSystem->loadBankFile(
+        name.c_str(),                      //filename of bank
+        FMOD_STUDIO_LOAD_BANK_NORMAL,      //Normal loading
+        &bank                              //Save pointer to bank
+    );
+
+    const int maxPathLength = 512;
+    if (result == FMOD_OK)
+    {
+        //Add bank to map
+        mBanks.emplace(name, bank);
+        //Load all non-streaming sample data
+        bank->loadSampleData();
+        //Get the number of events in this bank
+        int numEvents = 0;
+        bank->getEventCount(&numEvents);
+        
+        if (numEvents > 0)
+        {
+            //Get list of event description in this bank
+            std::vector<FMOD::Studio::EventDescription*> events(numEvents);
+            bank->getEventList(events.data(), numEvents, &numEvents);
+            char eventName[maxPathLength];
+            for (int i = 0; i < numEvents; i++)
+            {
+                FMOD::Studio::EventDescription* e = events[i];
+                //Get the path od this event (eg. event:/Explosion2D)
+                e->getPath(eventName, maxPathLength, nullptr);
+                //Add to event map
+                mEvents.emplace(eventName, e);
+            }
+        }
+    }
+}
+
+void AudioSystem::UnloadBank(const std::string& name)
+{
+    //Ignore if not loaded
+    auto iter = mBanks.find(name);
+    if (iter == mBanks.end())
+    {
+        return;
+    }
+
+    //First we need to remove all events from this bank
+    FMOD::Studio::Bank* bank = iter->second;
+    int numEvents = 0;
+    bank->getEventCount(&numEvents);
+    if (numEvents > 0)
+    {
+        // Get event descriptions for this bank
+        std::vector<FMOD::Studio::EventDescription*> events(numEvents);
+        // Get list of events
+        bank->getEventList(events.data(), numEvents, &numEvents);
+        char eventName[512];
+        for (int i = 0; i < numEvents; i++)
+        {
+            FMOD::Studio::EventDescription* e = events[i];
+            // Get the path of this event
+            e->getPath(eventName, 512, nullptr);
+            // Remove this event
+            auto eventToRemove = mEvents.find(eventName);
+            if (eventToRemove != mEvents.end())
+            {
+                mEvents.erase(eventToRemove);
+            }
+        }
+    }
+
+    int numBuses = 0;
+    bank->getBusCount(&numBuses);
+    if (numBuses > 0)
+    {
+        std::vector<FMOD::Studio::Bus*> buses(numBuses);
+        bank->getBusList(buses.data(), numBuses, &numBuses);
+        char busName[512];
+        for (int i = 0; i < numBuses; i++)
+        {
+            FMOD::Studio::Bus* bus = buses[i];
+            //Get the path of this bus eg (bus:/SFX)
+            bus->getPath(busName, 512, nullptr);
+            //Remove this bus
+            auto busToRemove = mBuses.find(busName);
+            if (busToRemove != mBuses.end())
+            {
+                mBuses.erase(busToRemove);
+            }
+        }
+    }
+    
+    //Unload sample data and bank
+    bank->unloadSampleData();
+    bank->unload();
+    //Remove from banks map
+    mBanks.erase(iter);
+}
+
+void AudioSystem::UnloadAllBanks(const std::string& name)
+{
+    for (auto& iter : mBanks)
+    {
+        //Unload the sample data, then the bank itself
+        iter.second->unloadSampleData();
+        iter.second->unload();
+    }
+
+    mBanks.clear();
+    //No banks, no events
+    mEvents.clear();
 }
--- project/AudioSystem.h
+++ project/AudioSystem.h
@@ -1,4 +1,9 @@
 #pragma once
+#include <unordered_map>
+#include <string>
+
+//SAMPLE DATA is the raw audio data that events reference. 
+//STREAMING DATA is sample data that streams into memory in small pieces at a time (MUSIC AND DIALOGUES)
 
 namespace FMOD
 {
@@ -23,10 +28,24 @@ class AudioSystem
 		void Shutdown();
 		void Update(float deltaTime);
 
+
+		void LoadBank(const std::string& name);
+		void UnloadBank(const std::string& name);
+		void UnloadAllBanks(const std::string& name);
+
 	private:
 		class Game* mGame;
 		//FMOD studio sys
 		FMOD::Studio::System* mSystem;
 		//fmod low level sys (if needed)
 		FMOD::System* mCoreSystem;
+
+		//Map of loaded banks
+		std::unordered_map<std::string, FMOD::Studio::Bank*> mBanks; //string is filename
+		//Map of event name to EventDescription
+		std::unordered_map<std::string, FMOD::Studio::EventDescription*> mEvents; //string is the name by fmod for the event
+		// Map of event id to EventInstance
+		std::unordered_map<unsigned int, FMOD::Studio::EventInstance*> mEventInstances;
+		// Map of buses
+		std::unordered_map<std::string, FMOD::Studio::Bus*> mBuses;
 };
